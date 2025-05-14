1f67bbb68fb3194f99bdb8642a6dd86db59203e8
Lakshay Vaishnav <thelakshayvaishnav@gmail.com>
2025-05-14 10:59:08 +0530

    basic prototype of primary backend completed

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,9 +2,11 @@ import express from "express"
 import userRouter from "./routes/user.js"
 import zapRouter from "./routes/zap.js"
 import actionRouter from "./routes/action.js"
+import cors from "cors"
 
 const app = express();
 app.use(express.json())
+app.use(cors())
 
 
 app.use("/api/v1/user", userRouter);
